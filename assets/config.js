7a57ce419afbac6c429edde4ece97f99e99c1032
patt-candice <nophfda.app@gmail.com>
2026-09-24 09:57:41 +0700

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+// ตั้งค่าองค์กรและตำแหน่ง API; บน Vercel ใช้พร็อกซีใน /api/gas
+window.SUPPLY_CONFIG = {
+  API_URL: '/api/gas',
+  ORGANIZATION: 'สำนักงานสาธารณสุขจังหวัดนครนายก',
+  GARUDA_URL: '',
+  STORAGE_TOKEN: 'supply_session_token',
+  STORAGE_MENU: 'supply_sidebar_hidden'
+};
